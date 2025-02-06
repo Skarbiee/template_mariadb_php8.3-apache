cfea184379eb045af0c518218f83ae349a79fe40
Skarbiee <garrigues.eva15@gmail.com>
2025-02-06 09:16:36 +0100

    update de la function deleteById

--- src/app/core/Database.php
+++ src/app/core/Database.php
@@ -1,22 +1,20 @@
 <?php
 class Database {
-    private $host = 'mariadb';           // hôte de la base de données
-    private $db_name = 'bdd_db';         // nom de la base de données
-    private $username = 'root';         // utilisateur de la base de données
-    private $password = '*';            // mot de lpasse de l'utilisateur
-    public $conn;                      // la connexion PDO
+    private $host = 'mariadb';      // hôte de la base de données
+    private $db_name = 'bdd_db';    // nom de la base de données
+    private $username = 'root';     // utilisateur de la base de données
+    private $password = '*';        // mot de lpasse de l'utilisateur
+    public $conn;                   // la connexion PDO
 
     // Méthode pour se connecter à la base de données
     public function connect(){
         try{
-            // PDO crée une connexion à la base de données
-            $this->conn = new PDO(
+            $this->conn = new PDO(                                                  // PDO crée une connexion à la base de données
             "mysql:host=$this->host;dbname=$this->db_name",
             $this->username,
             $this->password
         );
-            // On définit le mode d'erreur PDO en exception
-            $this->conn->setAttribute(
+            $this->conn->setAttribute(                                              // On définit le mode d'erreur PDO en exception
                 PDO::ATTR_ERRMODE,
                 PDO::ERRMODE_EXCEPTION
             );
--- src/app/core/Model.php
+++ src/app/core/Model.php
@@ -1,12 +1,4 @@
 <?php
-/**
- * @ Author: Mathéo Siron
- * @ Create Time: 2025-05-02 13:59:39
- * @ Modified by: Mathéo Siron
- * @ Modified time: 2025-05-02 13:59:39
- * @ Description: Classe de base de données pour gérer la connexion à la base de données.
- */
-
 class Model extends Database {
 
 
@@ -32,89 +24,79 @@ class Model extends Database {
 
     // Méthode pour exécuter la requête et récupérer les résultats
     public function get($index=null) {
-        $stmt = $this->conn->prepare($this->query);    // Préparation de la requête SQL
-        $stmt->execute($this->columns);                // Exécution de la requête
-        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);      // Retourner les résultats sous la forme d'un tableau associatif
+        $stmt = $this->conn->prepare($this->query); // Préparation de la requête SQL
+        $stmt->execute($this->columns);            // Exécution de la requête
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);       // Retourner les résultats sous la forme d'un tableau associatif
 
         if (is_null($index)) {
             return $result;
         }
-        return $result[$index];
+        return $result[$index];                            // Retourne le résultat sous forme de tableau
     }
 
 
     // Méthode pour sélectionner une colonne spécifique
     public function select($columns) {
         $this->query = "SELECT " . implode(',',(array)$columns) . " FROM " . $this->table;    // Construction de la requête avec les colonnes spécifiées
-        return $this;                                                                         // Permet de chainer les méthodes
+        return $this;                                                                                           // Permet de chainer les méthodes
     }
 
 
     // Méthode pour ajouter un enregistrement
     public function insert($data) {
-        $columns = implode(',', array_keys($data));                                             // Colonnes à insérer
-        $placeholders = ":" . implode(',;', array_keys($data));                                 // Placeholders pour les valeurs
+        $columns = implode(',', array_keys($data));                           // Colonnes à insérer
+        $placeholders = ":" . implode(',;', array_keys($data));               // Placeholders pour les valeurs
         $this->query = "INSERT INTO " . $this->table . " ($columns) VALUES ($placeholders)";    // Requête d'insertion
-        $stmt = $this->conn->prepare($this->query);                                             // Préparation de la requête
-
-        return $stmt->execute($data);                                                           // Exécution de la requête avec les données à insérer
+        $stmt = $this->conn->prepare($this->query);                                      // Préparation de la requête
+        return $stmt->execute($data);                                                   // Exécution de la requête avec les données à insérer
     }
-
-    // RELATION ENTRE LES TABLES
-    private function hasObject($relatedModel, $foreignKey) {
-        $related = new $relatedModel();                                                                       // Crée une instance du modèle lié
-        $relatedTable = $related->table;
-        $this->query .= " LEFT JOIN $relatedTable ON $relatedTable.$foreignKey = " . $this->table . ".id";
-        return $this;                                                                                         // Permet de chainer
-    }
-
-    // Méthode HasOne
-    public function hasOne($relatedModel, $foreignKey) {
-        return $this->hasObject($relatedModel, $foreignKey)->get(0);    // Récupère un résultat
-    }
-
-
-    // Méthode HasMany
-    public function hasMany($relatedModel, $foreignKey) {
-        return $this->hasObject($relatedModel, $foreignKey)->get();    // Récupère tout
-    }
-
 
     // Méthode Replace
     public function replace($data) {
         // On extrait les colonnes et leurs valeurs
-        $columns = implode(',', array_keys($data));                                               // Colonnes à insérer ou mettre à jour
-        $placeholders = ":" . implode(',:', array_keys($data));                                 // Placeholders pour les valeurs
-
-        // Construction de la requête REPLACE INTO
-        $this->query = "REPLACE INTO " . $this->table . " ($columns) VALUES ($placeholders)";
-
-        // Préparation et exécution de la requête
-        $stmt = $this->conn->prepare($this->query);
+        $columns = implode(',', array_keys($data));                                    // Colonnes à insérer ou mettre à jour
+        $placeholders = ":" . implode(',:', array_keys($data));                        // Placeholders pour les valeurs
+        $this->query = "REPLACE INTO " . $this->table . " ($columns) VALUES ($placeholders)";            // Construction de la requête REPLACE INTO
+        $stmt = $this->conn->prepare($this->query);                                               // Préparation et exécution de la requête
         return $stmt->execute($data);                                                            // Retourne true si l'opération a reussie
     }
 
     // Méthode delete
     public function delete() {
-        // Construction de la requête DELETE
-        $this->query = "DELETE FROM " . $this->table;
+        $this->query = "DELETE FROM " . $this->table;                  // Construction de la requête DELETE
 
         // Si des conditions where sont définies, on les ajoute à la requête
         if (!empty($this->columns)) {
             $this->query .= " WHERE " . key($this->columns) . " = " . key($this->columns);
         }
-
-        // Préparation et exécution de la requête
-        $stmt = $this->conn->prepare($this->query);
-        return $stmt->execute($this->columns);                                                // Retourne true si l'opération a reussie
+        $stmt = $this->conn->prepare($this->query);             // Préparation et exécution de la requête
+        return $stmt->execute($this->columns);                 // Retourne true si l'opération a reussie
     }
 
     // Méthode BelongTo
-    public function beongTo($relatedModel, $foreignKey) {
-        $related = new $relatedModel();                                                                          // Crée une instance du modèle lié
+    public function belongTo($relatedModel, $foreignKey) {
+        $related = new $relatedModel();                            // Crée une instance du modèle lié
         $relatedTable = $related->table;
         $this->query .= " LEFT JOIN $relatedTable ON $relatedTable.id = " . $this->table . "." . $foreignKey;
-        return $this;                                                                                            // Permet de chainer
+        return $this;                                              // Permet de chaîner
+    }
+
+    // RELATION ENTRE LES TABLES
+    private function hasObject($relatedModel, $foreignKey) {
+        $related = new $relatedModel();                              // Crée une instance du modèle lié
+        $relatedTable = $related->table;
+        $this->query .= " LEFT JOIN $relatedTable ON $relatedTable.$foreignKey = " . $this->table . ".id";
+        return $this;                                                // Permet de chaîner
+    }
+
+    // Méthode hasOne
+    public function hasOne($relatedModel, $foreignKey) {
+        return $this->hasObject($relatedModel, $foreignKey)->get(0);    // Récupère un résultat
+    }
+
+    // Méthode hasMany
+    public function hasMany($relatedModel, $foreignKey) {
+        return $this->hasObject($relatedModel, $foreignKey)->get();    // Récupère tout
     }
 }
 ?>
