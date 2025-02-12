ced03e6d593627a18f023d0cf40a579246e55bb4
Skarbiee <garrigues.eva15@gmail.com>
2025-02-12 14:49:39 +0100

    update router et display errors

--- src/public/index.php
+++ src/public/index.php
@@ -1,8 +1,9 @@
 <?php
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+require_once '../app/core/Functions.php';
+displayErrorsPHP();
+
+dd(getenv());
 
 $request = $_SERVER['REQUEST_URI'];
 
@@ -11,12 +12,12 @@ function includeComponent($component){
     include '../app/views/layout/header.php';
     include '../app/views/' . $component . '.php';
     include '../app/views/layout/footer.php';
-
 }
+
 
 switch($request){
     case '/':
-    case '/home';
+    case '/home':
         includeComponent('home');
         break;
     case '/contact':
