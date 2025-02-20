b1b6fa8ad10c5059067779ee0757601e7d92569c
Skarbiee <garrigues.eva15@gmail.com>
2025-02-20 14:49:35 +0100

    Ajout de namespace et exemple Controller

---
+++ src/app/core/Autoloader.php
@@ -0,0 +1,33 @@
+<?php
+namespace App\Core;
+
+class Autoloader
+{
+    /**
+     * Enregistre la fonction Autoload avec le registre SPL Autoload.
+     * Cela permet à la classe AutolOader de charger automatiquement les classes
+     * Comme ils sont référencés tout au long de la demande.
+     */
+    public static function register()
+    {
+        spl_autoload_register([__CLASS__, 'autoload']);
+    }
+
+    // autoload.php : Function d'autoloading personnalisée
+    function autoload($className)
+    {
+        // Remplace les \ par des / pour la compatibilité avec les chemins de fichiers
+        $className = str_replace('\\', '/', $className);
+
+        // Définis le chemin de base pour les classes
+        $basePath = __DIR__ . '../../'; // Dossier des classes dans le projet
+
+        // Construis le chemin complet vers la classe
+        $filePath = $basePath . $className . '.php';
+
+        // Vérifie si le fichier existe et inclut-le
+        if (file_exists($filePath)) {
+            require_once $filePath;
+        }
+    }
+}
--- src/app/models/UserModel.php
+++ src/app/models/UserModel.php
@@ -1,4 +1,8 @@
 <?php
+namespace App\Models;
+
+use App\Core\Model;
+
 class UserModel extends Model
 {
     protected $table = 'users'; // nom de la table
